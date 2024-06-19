66c7a5a1f56c53b66d101ae18627a2262a4858ca
Kaltrina Ajvazi <73668773+KaltrinaAjvazi@users.noreply.github.com>
2024-06-19 22:36:06 +0200

    Update Employee.js

--- Project/src/Employee.js
+++ Project/src/Employee.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react';
 import {Table} from 'react-bootstrap';
-
 import {Button,ButtonToolbar} from 'react-bootstrap';
 import {AddEmpModal} from './AddEmpModal';
 import {EditEmpModal} from './EditEmpModal';
